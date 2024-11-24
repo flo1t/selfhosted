51fb6478fe6e3f4c73b37cfb1b4c8d370c9b7903
florian <florian.rutishauser@gmail.com>
2024-11-24 15:54:06 +0100

    feature: gitea action to remove public ip from crowdsec

--- hosts/all/scripts/ssh-login.sh
+++ hosts/all/scripts/ssh-login.sh
@@ -3,9 +3,16 @@
 
 # DISCORD_HIGHSECURITY_URL is set in the .env file
 discord_url=$DISCORD_HIGHSECURITY_URL
+discord_low_url=$DISCORD_LOWSECURITY_URL
 
 if [ "${PAM_TYPE}" = "open_session" ]; then
-  message="SSH login: ${PAM_USER} from ${PAM_RHOST}"
-  json_payload="{\"content\":\"$message\",\"username\":\"$HOSTNAME\"}"
-  curl -H "Content-Type: application/json" -d "$json_payload" "$discord_url"
+  if [ "${PAM_RHOST}" = $RUNNER_IP ]; then # exclude runner ip from high alerts
+    message="SSH login: ${PAM_USER} from ${PAM_RHOST}"
+    json_payload="{\"content\":\"$message\",\"username\":\"$HOSTNAME\"}"
+    curl -H "Content-Type: application/json" -d "$json_payload" "$discord_low_url"
+  else
+    message="SSH login: ${PAM_USER} from ${PAM_RHOST}"
+    json_payload="{\"content\":\"$message\",\"username\":\"$HOSTNAME\"}"
+    curl -H "Content-Type: application/json" -d "$json_payload" "$discord_url"
+  fi
 fi
