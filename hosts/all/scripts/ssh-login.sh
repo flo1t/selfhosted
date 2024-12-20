594062d3c7359d09f432fb94e1ade8785139bb0e
florian <florian.rutishauser@gmail.com>
2024-12-20 15:03:00 +0100

    feature: change wg-easy image, update postgres,mariadb

--- hosts/all/scripts/ssh-login.sh
+++ hosts/all/scripts/ssh-login.sh
@@ -3,16 +3,9 @@
 
 # DISCORD_HIGHSECURITY_URL is set in the .env file
 discord_url=$DISCORD_HIGHSECURITY_URL
-discord_low_url=$DISCORD_LOWSECURITY_URL
 
 if [ "${PAM_TYPE}" = "open_session" ]; then
-  if [ "${PAM_RHOST}" = $RUNNER_IP ]; then # exclude runner ip from high alerts
-    message="SSH login: ${PAM_USER} from ${PAM_RHOST}"
-    json_payload="{\"content\":\"$message\",\"username\":\"$HOSTNAME\"}"
-    curl -H "Content-Type: application/json" -d "$json_payload" "$discord_low_url"
-  else
-    message="SSH login: ${PAM_USER} from ${PAM_RHOST}"
-    json_payload="{\"content\":\"$message\",\"username\":\"$HOSTNAME\"}"
-    curl -H "Content-Type: application/json" -d "$json_payload" "$discord_url"
-  fi
+  message="SSH login: ${PAM_USER} from ${PAM_RHOST}"
+  json_payload="{\"content\":\"$message\",\"username\":\"$HOSTNAME\"}"
+  curl -H "Content-Type: application/json" -d "$json_payload" "$discord_url"
 fi
